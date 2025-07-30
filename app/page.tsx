11e370365e77e0af07c24e735cf784abe4ee5c5f
Tofikbe <karategamingyt@gmail.com>
2025-07-30 15:36:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
 import Header from './components/Header'
+
+export default function HomePage() {
+  return (
+    <main className="text-white">
+      <h1>Welcome to QurixAI</h1>
+    </main>
+  );
+}
