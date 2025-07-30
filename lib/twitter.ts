a72ff3e4a1b8cbf7aa5bf180a51e98cc495d94bc
Tofikbe <karategamingyt@gmail.com>
2025-07-30 14:38:47 +0530

    Update twitter.ts

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -1,1 +1,7 @@
-
+export async function fetchTweetsFromX() {
+  // Dummy data for testing
+  return [
+    { id: '1', text: 'Love building on Aptos!' },
+    { id: '2', text: 'Polygon is scaling fast 🚀' },
+  ]
+}
