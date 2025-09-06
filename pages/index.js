bc75bf0553fae00d6bc74f1e1cbb0e29bb6564a2
Tofikbe <karategamingyt@gmail.com>
2025-09-06 09:56:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+import ChatUI from "../components/ChatUI";
 
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <ChatUI />
+    </div>
+  );
+}
