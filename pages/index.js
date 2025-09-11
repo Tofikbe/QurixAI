4efae1e574e7366338d4716418d8b82158c998c2
Tofikbe <karategamingyt@gmail.com>
2025-09-11 18:28:01 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">QurixAI Chatbot</h1>
-      <p className="mt-4">Chat UI will appear here.</p>
-    </div>
-  );
-}
